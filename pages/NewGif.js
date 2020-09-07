20d9929059b907e185b297166bdb35814a9030c4
Camila Urquizo <curquizo@nuskin.com>
2020-09-06 20:34:06 -0500

    Update:Camera fix on mobile -new logic

--- pages/NewGif.js
+++ pages/NewGif.js
@@ -1,36 +1,58 @@
 const viewNewGif = `
+<div class="flex">
+<div class="flex-1 onDesktop film-camara">
+   
+    <div class="film-camara-container">
+        <div class="film-camara-record">
+            <img class="record1" src="./assets/element_cinta1.svg" alt="">
+            <img class="record2" src="./assets/element_cinta2.svg" alt="">
+        </div>
+        <div class="film-camara-body">
+            <img class="film-camara-main" src="./assets/element-camara.svg" alt="">
+            <img class="film-camara-light" src="./assets/element-luz-camara.svg" alt="">
+        </div>
+    </div>
+
+</div>
+<div class="flex-2">
     <div class="new-gif-create-container">
-    <div class="flex new-gifs-square">
-        <div class="square top-border left-border"></div>
-        <div class="square top-border right-border"></div>
-    </div>
-    <div class="flex new-gifs-square">
-        <div class="square botom-border left-border"></div>
-        <div class="square botom-border right-border"></div>
-    </div>
-    <div id="video-container" class="new-gifos-welcome-message">
-        <h1  class="main-title">Aquí podrás <br> crear tus propios <span>GIFOS</span></h1>
-        <p class="new-gifos-welcome-paragraph">¡Crea tu GIFO en sólo 3 pasos!<br>(sólo necesitas una cámara para grabar un video)</p>
-    </div>
+        <div class="flex new-gifs-square">
+            <div class="square top-border left-border"></div>
+            <div class="square top-border right-border"></div>
+        </div>
+        <div class="flex new-gifs-square">
+            <div class="square botom-border left-border"></div>
+            <div class="square botom-border right-border"></div>
+        </div>
+        <div id="video-container" class="new-gifos-welcome-message">
+            <h1 class="main-title">Aquí podrás <br> crear tus propios <span>GIFOS</span></h1>
+            <p class="new-gifos-welcome-paragraph">¡Crea tu GIFO en sólo 3 pasos!<br>(sólo necesitas una cámara para grabar un video)</p>
+        </div>
     </div>
     <div class="new-gif-create-controllers">
-    <div class="new-gif-create-steps"">
-        <ul class="new-create-steps">
-            <li id="step1" class="step">1</li>
-            <li id="step2" class="step">2</li>
-            <li id="step3"class="step">3</li>
-            <li id="time-capture" class="onDesktop capture-time">Repetir Captura</li>
-        </ul>
-        
+        <div class="new-gif-create-steps"">
+            <ul class="new-create-steps">
+                <li id="step1" class="step">1</li>
+                <li id="step2" class="step">2</li>
+                <li id="step3"class="step">3</li>
+                <li id="time-capture" class="onDesktop capture-time">Repetir Captura</li>
+            </ul>
+            
+        </div>
+        <div class="new-gif-line-divider"></div>
+        <div class="new-gif-record-container">
+            <div id="start" class="button">Comenzar</div>
+            <div id="record" class="button no-display">Grabar</div>
+            <div id="finish" class="button no-display">Finalizar</div>
+            <div id="upload" class="button no-display">Descargar Gifo</div>
+        </div>
     </div>
-    <div class="new-gif-line-divider"></div>
-    <div class="new-gif-record-container">
-        <div id="start" class="button">Comenzar</div>
-        <div id="record" class="button no-display">Grabar</div>
-        <div id="finish" class="button no-display">Finalizar</div>
-        <div id="upload" class="button no-display">Descargar Gifo</div>
-    </div>
-    </div>
+</div>
+<div class="flex-1 movie onDesktop">
+    <img src="./assets/pelicula.svg" alt="">
+</div>
+
+</div>
 `;
 
 const giveAccesToCamara = `
